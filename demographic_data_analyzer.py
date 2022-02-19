001ee177db29b485d07b1f68e63d56ef78979611
Michael Green-Lab Computer <mjgreen2g@yahoo.com>
2022-02-18 19:06:25 -0600

    Update solution to experiment with different pandas concepts

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -19,7 +19,8 @@ def calculate_demographic_data(print_data=True):
 
     # What is the percentage of people who have a Bachelor's degree?
     bach = pd.DataFrame(df[df['education'] == 'Bachelors'])
-    percentage_bachelors = round(np.size(bach['education'])/np.size(df['education']) * 100, 1)
+    percentage_bachelors = round(np.size(bach['education'])
+                            /np.size(df['education']) * 100, 1)
     
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
@@ -27,48 +28,58 @@ def calculate_demographic_data(print_data=True):
 
     # with `Bachelors`, `Masters`, or `Doctorate`
     adv_degree = pd.DataFrame(df[df['education'] == 'Masters'])
-    adv_degree = pd.concat([adv_degree, df[df['education'] == 'Bachelors'], df[df['education'] == 'Doctorate']])
-    higher_education = np.size(adv_degree['education'])/np.size(df['education']) * 100
+    adv_degree = pd.concat([adv_degree, 
+                            df[df['education'] == 'Bachelors'], 
+                            df[df['education'] == 'Doctorate']])
+    higher_education_percentage = (np.size(adv_degree['education'])
+                                    /np.size(df['education']) * 100)
 
 
     # without `Bachelors`, `Masters`, or `Doctorate`
-    no_college = df[(df['education'] != 'Bachelors') & (df['education'] != 'Masters') & (df['education'] != 'Doctorate')]
-    lower_education = np.size(no_college['education'])/np.size(df['education']) 
+    no_college = df[(df['education'] != 'Bachelors') 
+                    & (df['education'] != 'Masters') 
+                    & (df['education'] != 'Doctorate')]
+    lower_education_percentage = (np.size(no_college['education'])
+                                    /np.size(df['education']))
 
 
     # percentage with salary >50K
-    money_degree =pd.DataFrame(adv_degree[adv_degree['salary'] == '>50K'])
-    higher_education_rich = round(np.size(money_degree['salary'])/np.size(adv_degree['salary']) * 100, 1)
+    rich_degree =pd.DataFrame(adv_degree[adv_degree['salary'] == '>50K'])
+    higher_education_rich = round(np.size(rich_degree['salary'])
+                                    /np.size(adv_degree['salary']) * 100, 1)
 
-    money_no_college = no_college[no_college['salary'] == '>50K']
-    lower_education_rich = round(np.size(money_no_college['education'])/np.size(no_college['education']) * 100, 1)
+    rich_no_college = no_college[no_college['salary'] == '>50K']
+    lower_education_rich = round(np.size(rich_no_college['education'])
+                                    /np.size(no_college['education']) * 100, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
     min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    min_hours = df['hours-per-week'].min()
-    num_min_workers = df[df['hours-per-week'] == min_hours]
-
-    money_min_workers = num_min_workers[num_min_workers['salary'] == '>50K']
-    rich_percentage = np.size(money_min_workers['salary'])/np.size(num_min_workers['salary']) * 100
+    total_min_workers = pd.DataFrame(df[(df['hours-per-week'] 
+                                            == min_work_hours)], 
+                                            columns=['salary'])
+    rich_min_workers  = pd.DataFrame(total_min_workers[
+                                    total_min_workers['salary'] == '>50K'], columns=['salary'])
+    rich_percentage = (np.size(rich_min_workers['salary'])
+                        /np.size(total_min_workers['salary']) 
+                        * 100)
 
     # What country has the highest percentage of people that earn >50K?
-    money_people = df[df['salary'] == '>50K']
-    money_list = pd.Series(money_people['native-country'].value_counts(), None, object)
-    pop_list = pd.Series(df['native-country'].value_counts(), None, object)
-    percent_list = money_list/pop_list
-    highest_earner = dict(percent_list[percent_list == percent_list.max()])
-    highest_earning_country = list(highest_earner)[0]
+    rich_people = df[df['salary'] == '>50K']
+    country_rich_pop = pd.Series(rich_people['native-country'].value_counts(),                              None, object)
+    country_total_pop = pd.Series(df['native-country'].value_counts(), 
+                                    None, object)
+    country_rich_percentage = country_rich_pop/country_total_pop
+    highest_earner = country_rich_percentage[country_rich_percentage == country_rich_percentage.max()]
+    highest_earning_country = highest_earner.index[0]
     highest_earning_country_percentage = round(highest_earner.get(highest_earning_country) * 100, 1)
 
     # Identify the most popular occupation for those who earn >50K in India.
-    indians = df[df['native-country'] == 'India']
-    money_indians = indians[indians['salary'] == '>50K']
-    jobs_of_money_indians = pd.Series(money_indians['occupation'].value_counts(), None, object)
-    highest_indian_earner = dict(jobs_of_money_indians[jobs_of_money_indians == jobs_of_money_indians.max()])
-    top_IN_occupation = list(highest_indian_earner)[0]
-
+    rich_indians = pd.DataFrame(df[(df['native-country'] == 'India') 
+                    & (df['salary'] == '>50K')], columns=['occupation']).value_counts(sort=True)
+    top_IN_occupation = rich_indians.idxmax()[0]
+    
     # DO NOT MODIFY BELOW THIS LINE
 
     if print_data:
